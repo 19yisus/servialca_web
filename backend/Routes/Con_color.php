cf9b9e6c1112ba2c1a6a41c72675e6733411e7c3
Ricardo <ricardofenomeno13@gmail.com>
2023-08-08 21:11:54 -0400

    creo que lo hice bien

--- backend/Routes/Con_color.php
+++ backend/Routes/Con_color.php
@@ -4,7 +4,6 @@ require_once("./Models/cls_color.php");
 class Con_color extends cls_color
 {
 
-<<<<<<< HEAD
     public function ConsultarTodos()
     {
         $resultado = $this->GetAll();
@@ -12,11 +11,11 @@ class Con_color extends cls_color
     }
 }
 
-=======
+
 	public function ConsultarTodos()
 	{
 		$resultado = $this->GetAll();
 		Response($resultado, 200);
 	}
 }
->>>>>>> 70525dad285d7ff7a2cb25f9b9aff632356bc29d
+
