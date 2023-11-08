b2685b13a040902808f5bb9d63abc4aedca054c9
Ricargame <ricardofenomeno13@gmail.com>
2023-11-08 19:51:11 -0400

    Guardar cambios en datos.js

--- frontend/src/modulos/datos.js
+++ frontend/src/modulos/datos.js
@@ -8,8 +8,8 @@ switch (municipio) {
     //host = 'http://192.168.88.76'
     // host = 'http://localhost' 
     // host = 'http://localhost' 
-    //host = 'http://localhost:80/servialca_web/backend'
-    host = 'https://servialcarcv.com/servialca_web/backend'
+    host = 'http://localhost:80/servialca_web/backend'
+    //host = 'https://servialcarcv.com/servialca_web/backend'
     
     // host = 'http://192.168.0.110'
     module.exports = {
